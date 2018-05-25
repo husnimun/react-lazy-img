badaa659141d302ecd3c1cafb44ed3a375912133
Husni Munaya <husnimunaya@gmail.com>
2018-05-25 22:50:39 +0700

    Add demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,47 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-import Example from '../../src'
+import LazyImg from '../../src/index'
+
+const ImageWrapper = ({ children }) => (
+  <div style={{ width: '500px', height: '500px' }}>{children}</div>
+)
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-lazy-img Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>React Lazy Image Demo</h1>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?nature" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?city" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?sky" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?beach" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?night" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?sunset" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?sunrise" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?stars" />
+        </ImageWrapper>
+        <ImageWrapper>
+          <LazyImg src="https://source.unsplash.com/random/500x500?car" />
+        </ImageWrapper>
+      </div>
+    )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
